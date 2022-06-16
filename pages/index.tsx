3b506dc3b9850195b11cd3e96090bd7aa2aa1dac
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-06-16 11:15:55 +0200

    Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +10,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Kittisaurus personality test" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-5xl">Hello World</h1>
+      <main className="bg-gradient-to-br from-rose-200 to-orange-200 min-h-screen flex items-center">
+        <section className="max-w-screen-md mx-auto px-4 md:px-0 flex flex-col items-center gap-4">
+          <h1 className="text-xl lg:text-2xl xl:text-3xl font-semibold text-gray-900 text-center">Kittisaurus personality test</h1>
+          <p className="lg:text-lg xl:text-xl text-gray-700 text-center">Which Kittisaurus cat are you? Let{"’"}s find out!</p>
+          <Link href="/test">
+            <a className="btn btn-primary">Take the test</a>
+          </Link>
+        </section>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
