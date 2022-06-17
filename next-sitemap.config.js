a161a1a559f011e818aaba71d36af1b87bd31405
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-06-17 10:26:13 +0200

    Add sitemap & robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://kittisaurus.hynekfisera.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
+
+export default config;
