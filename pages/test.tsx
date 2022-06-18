bc843ce26cf79072c45be6f419fd97ded3ae19c3
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-06-18 17:02:45 +0200

    Test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from "next-seo";
 import questions from "../resources/questions";
 import { Question } from "../types/Question";
 import { Points } from "../types/Points";
+import Router from "next/router";
 
 type Props = {};
 
@@ -12,21 +13,60 @@ export default function Test({}: Props) {
   const [points, setPoints] = useState<Points[]>([]);
 
   useEffect(() => {
+    let temp: Points[] = [];
     for (let i = 1; i <= 10; i++) {
-      points.push({
+      temp.push({
         catId: i,
         points: 0,
         maxPoints: 0,
       });
     }
+    setPoints(temp);
   }, []);
 
   useEffect(() => {
     setQuestion(questions[number]);
   }, [number]);
 
-  const onAnswer = (id: number) => {
-    setNumber((n) => n + 1);
+  const onAnswer = (i: number, count: number) => {
+    let temp = points;
+
+    // update points
+    temp = temp.map((cat) => {
+      if (question.answers[i].pointsFor.includes(cat.catId)) {
+        return { ...cat, points: cat.maxPoints + count };
+      } else {
+        return cat;
+      }
+    });
+
+    // update max points
+    temp = temp.map((cat) => {
+      if (question.answers.some((answer) => answer.pointsFor.includes(cat.catId))) {
+        return { ...cat, maxPoints: cat.maxPoints + 1 };
+      } else {
+        return cat;
+      }
+    });
+
+    // set new points
+    setPoints(temp);
+
+    // check if another question exists
+    if (questions.length - 1 === number) {
+      let url = "";
+      points.forEach((cat, i) => {
+        url += `${cat.catId}-${cat.points}-${cat.maxPoints}`;
+        if (i !== points.length - 1) {
+          url += ",";
+        }
+      });
+      // redirect to result
+      Router.push(`/result/${url}`);
+    } else {
+      // go to next question
+      setNumber((n) => n + 1);
+    }
   };
 
   return (
@@ -36,16 +76,26 @@ export default function Test({}: Props) {
         <section className="max-w-screen-md mx-auto px-4 md:px-0 flex flex-col items-center gap-4">
           <h1 className="text-xl lg:text-2xl xl:text-3xl font-semibold text-gray-900 text-center">{question.title}</h1>
           <div className="flex justify-center gap-4">
-            {question.answers.map((answer, i) => (
-              <div
-                key={i}
-                onClick={() => {
-                  onAnswer(i);
-                }}
-              >
-                {answer.title}
-              </div>
-            ))}
+            {question.answers.length === 2 && (
+              <>
+                <div onClick={() => onAnswer(0, 1)}>{question.answers[0].title}</div>
+                <div onClick={() => onAnswer(0, 0.5)}>+</div>
+                <div onClick={() => onAnswer(0, 0)}>.</div>
+                <div onClick={() => onAnswer(1, 0.5)}>-</div>
+                <div onClick={() => onAnswer(1, 1)}>{question.answers[1].title}</div>
+              </>
+            )}
+            {question.answers.length !== 2 &&
+              question.answers.map((answer, i) => (
+                <div
+                  key={i}
+                  onClick={() => {
+                    onAnswer(i, 1);
+                  }}
+                >
+                  {answer.title}
+                </div>
+              ))}
           </div>
         </section>
       </main>
