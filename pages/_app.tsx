a2cea8100915d8aeaf1007bdbd86ac9627d3e76e
Hynek Fišera <hynek.fisera@seznam.cz>
2022-11-28 08:53:02 +0100

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
